15f5fda227e92a1680fcb9d689bb9f3bd3a82f1b
ChiaraPassaro <chiarapassaro.wd@gmail.com>
2024-03-10 08:56:41 +0100

    feat: added js ex 1 prices base

---
+++ ex-js-1-prices/js/script.js
@@ -0,0 +1,23 @@
+const baseNumberUsers = 10;
+const baseGb = 2;
+
+const usersIncludedString = 'users included';
+const gbIncludedString = 'GB of storage';
+
+//Tenuto conto che
+//il piano free ha costo 0, 
+//users 10
+//gb 2
+
+//inserisci questi dati negli elementi del dom con l'id giusto.
+
+//Il piano Pro
+//costa 15$, ha il ha il doppio degli utenti e cinque volte in piu di GB 
+
+//inserisci questi dati negli elementi del dom con l'id giusto.
+
+//Il piano Enterprise
+//Costa il doppio del pro, ha il triplo degli utenti e otto volte in piu di GB
+//Calcola il numero di utenti e di GB, poi stampali in pagina
+
+//inserisci questi dati negli elementi del dom con l'id giusto.
