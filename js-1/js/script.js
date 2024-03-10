499ac091e70e3798dc9456ca8c7bdec28c31c64b
ChiaraPassaro <chiarapassaro.wd@gmail.com>
2024-03-10 08:56:19 +0100

    feat: added livecoding js 1

---
+++ js-1/js/script.js
@@ -0,0 +1,68 @@
+// // alert('Ciao');
+
+// document.getElementById('output').innerHTML = 'Ciao Mondo';
+// console.log('Ciao Ciao');
+
+// const firstName = 'Chiara';
+// const lastName = 'Passaro';
+// let userName = 'Chiara1980';
+// console.log(userName);
+
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+// userName = 'Kja1980';
+// console.log(userName);
+
+
+// // firstName = 'Sandra';
+
+// const string = 'L\'albero';
+
+// const fullName = firstName + ' ' + lastName;
+// console.log(fullName);
+
+// const number1 = 10;
+// const number2 = 5;
+// const somma = number1 + number2;
+// console.log(somma);
+// console.log('15');
+// const string1 = 'Chiara';
+// const newSum = string1 * number2;
+
+// console.log(newSum);
+
+
+// const product1 = 10.50;
+// const quantity1 = 4;
+// const total1 = product1 * quantity1;
+
+// const product2 = 3;
+// const quantity2 = 1;
+// const total2 = product2 * quantity2;
+
+// const product3 = 200;
+// const quantity3 = 2;
+// const total3 = product3 * quantity3;
+
+// // const total = product1 + product2 + product3;
+// const total = total1 + total2 + total3;
+// console.log(total);
+
+const nameProduct = 'Occhiale da sole Blue';
+const nameProduct2 = 'TShirt Bianca';
+const nameProduct3 = 'Scarpe da Ginnastica';
+
+document.getElementById('product1').innerHTML = nameProduct;
+document.getElementById('product2').innerHTML = nameProduct2;
+document.getElementById('product3').innerHTML = nameProduct3;
+
+const price1 = 200.50;
+document.getElementById('price1').innerHTML = price1 + '€';
+const price2 = 60;
+document.getElementById('price2').innerHTML = price2 + '€';
+const price3 = 90;
+document.getElementById('price3').innerHTML = price3 + '€';
+
+document.getElementById('total-price').innerHTML = price1 + price2 + price3 + '€';
