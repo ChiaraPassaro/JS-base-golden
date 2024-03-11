60db2842e80615c89594c10e586c505d510d7f4c
ele141082 <egn1982@gmail.com>
2024-03-11 18:47:01 +0100

    feat: added free and pro price.

--- ex-js-1-prices/js/script.js
+++ ex-js-1-prices/js/script.js
@@ -1,8 +1,50 @@
+//free-price
 const baseNumberUsers = 10;
 const baseGb = 2;
+const basePrice = 0;
+const value = "$/mo";
+
+
+const stringBasePrice = basePrice + value;
+document.getElementById("free-price").innerHTML = stringBasePrice;
 
 const usersIncludedString = 'users included';
 const gbIncludedString = 'GB of storage';
+
+//alert ("Ciao");
+
+const stringGbCompleted = baseGb + " " + gbIncludedString;
+console.log(stringGbCompleted);
+
+document.getElementById("free-gb").innerHTML = stringGbCompleted;
+
+const stringUsersCompleted = baseNumberUsers + " " + usersIncludedString;
+console.log(stringUsersCompleted);
+
+document.getElementById("free-users").innerHTML = stringUsersCompleted;
+
+//pro-price
+let proNumberUsers = baseNumberUsers * 2;
+proNumberUsers = 20;
+console.log(proNumberUsers);
+const proPrice = 15;
+let proGb = baseGb * 5;
+proGb = 10;
+console.log(proGb);
+
+const stringProPrice = proPrice + value;
+document.getElementById("pro-price").innerHTML = stringProPrice;
+
+const stringProUsersCompleted = proNumberUsers + " " + usersIncludedString;
+console.log(stringProUsersCompleted);
+document.getElementById("pro-users").innerHTML = stringProUsersCompleted;
+
+const stringProGbCompleted = proGb + " " + gbIncludedString;
+console.log(stringProGbCompleted);
+document.getElementById("pro-gb").innerHTML = stringProGbCompleted;
+
+
+
 
 //Tenuto conto che
 //il piano free ha costo 0, 
