a197cd2fe55e6327d40bef4b662c335d66fac06a
ele141082 <egn1982@gmail.com>
2024-03-22 07:47:47 +0100

    feat: added button

--- ex-js-dom-consegna/hamburger/js/script.js
+++ ex-js-dom-consegna/hamburger/js/script.js
@@ -1,1 +1,28 @@
 //Crea uno script che premendo sul bottone hamburger, mostra il menu mobile, e premendo su una X chiude il menu mobile
+
+const button = document.getElementById("hamburger");
+console.log("button");
+const buttonClose = document.getElementById("close");
+console.log("button");
+const p = document.querySelector("p.hidden");
+
+button.addEventListener("click",
+ function () {
+ p.classList.remove("hidden");
+ }
+
+
+)
+
+buttonClose.addEventListener("click",
+function () {
+p.classList.add("hidden");
+
+
+}
+
+)
+
+
+
+
