4990a5531066151da6ca7c1a548c5cde9dc11339
ChiaraPassaro <chiarapassaro.wd@gmail.com>
2024-04-13 17:08:28 +0200

    feat: live if

---
+++ js3-istruzioni-condizionali/js/script.js
@@ -0,0 +1,69 @@
+// console.log(3 > 2);
+
+// if (3 < 2) {
+//   console.log('tre minore di due');
+// } else {
+//   console.log('tre maggiore di due');
+// }
+
+
+// const name1 = 'Chiara';
+// const name2 = 'Chiara';
+
+// if (name1 == name2) { // true
+//   console.log('sono uguali');
+// } else {
+//   console.log('non sono uguali');
+// }
+
+// 1 === '1'
+
+// Controllo dell'età: Chiedi all'utente la sua età. Se è maggiore di 18 anni, stampa "Accesso consentito", altrimenti "Accesso negato".
+
+// const eta = prompt('Inserisci la tua età');
+// if (eta >= 18) {
+//   console.log('Puoi entrare');
+// } else {
+//   console.log('Non puoi entrare');
+// // }
+// const inputEta = document.getElementById('eta');
+// const button = document.getElementById('submit');
+
+// button.addEventListener('click',
+//   function () {
+//     const valueInput = inputEta.value;
+//     if (valueInput >= 18) {
+//     console.log('Puoi entrare');
+//     } else {
+//       console.log('Non puoi entrare');
+//     }
+//   }
+// )
+
+// 2. Parola più lunga: Chiedi all'utente di inserire due parole. Stampa la parola più lunga.
+
+// const string1 = 'Pippo';
+// console.log(string1.length);
+
+// const parola1 = prompt('Inserisci la prima parola');
+// const parola2 = prompt('Inserisci la seconda parola');
+
+// if (parola1.length > parola2.length) {
+//   console.log(parola1);
+// } else if (parola1.length == parola2.length) {
+//   console.log('hanno la stessa lunghezza');
+// } else { //else e opzionale
+//   console.log(parola2);
+// }
+
+// Controllo lunghezza password: Chiedi all'utente di inserire una password. Se ha meno di 8 caratteri, stampa "Password troppo corta", altrimenti "Password valida".
+
+// const password = prompt('inserisci la password');
+
+// if (password.length < 8) {
+//   console.log('Password troppo corta');
+// } else {
+//   console.log('Password valida');
+// }
+
+
