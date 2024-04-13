8b97d1ef7fb47b1fcffb4b06aa3a05390b25faf7
ele141082 <egn1982@gmail.com>
2024-04-13 17:45:43 +0200

    feat: added if and else

--- ex-js-dom-consegna/carrello/js/script.js
+++ ex-js-dom-consegna/carrello/js/script.js
@@ -3,3 +3,54 @@
 
 
 //Bonus: Aggiungi una riga con il prezzo totale di tutti i prodotti nel carrello
+
+alert ("ciao ciao")
+
+const orange = document.getElementById("orange");
+console.log("orange");
+
+const blueberries = document.getElementById("blueberries");
+console.log("blueberries");
+
+const buttonAdd = document.querySelector("btn");
+console.log("buttonAdd");
+
+const quantityOrange = 1;
+
+const priceOrange = 3;
+
+
+
+const temperatura = prompt("inserisci temperatura");
+
+if (temperatura < 0) {
+    console.log("è sotto lo zero");
+
+} if(temperatura > 30){
+    console.log("é sopra i 30 gradi");
+
+}else {
+    console.log("temperatura normale");
+}
+
+const parola1 = prompt("inserisci una stringa");
+const parola2 = prompt("inserisci una stringa")
+
+if (parola1 === parola2){
+    console.log("le stringhe sono uguali");
+
+} else {
+    console.log("le stringhe sono diverse");
+}
+
+const number = prompt("inserisci un numero");
+
+if (number > 0 ) {
+    console.log("il numero è positivo");
+
+}else if (number < 0 ) {
+    console.log("il numero è negativo");
+
+}else {
+    console.log("il numero è zero");
+}
