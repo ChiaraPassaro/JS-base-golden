8e149394d12831c2c84440f8dca276ebe2bae5ea
ChiaraPassaro <chiarapassaro.wd@gmail.com>
2024-04-13 15:57:00 +0200

    feat: added exercises

--- ex-js-dom-consegna/carrello/js/script.js
+++ ex-js-dom-consegna/carrello/js/script.js
@@ -3,3 +3,66 @@
 
 
 //Bonus: Aggiungi una riga con il prezzo totale di tutti i prodotti nel carrello
+const priceOrange = 1.50;
+const priceBlueberries = 2.00;
+
+//'#orange .add-item'
+//'#blueberries .add-item'
+
+const buttonAddItemOrange = document.querySelector('#orange .add-item');
+const buttonAddItemBlueberries = document.querySelector('#blueberries .add-item');
+const buttonRemoveItemOrange = document.querySelector('#orange .remove-item');
+const buttonRemoveItemBlueberries = document.querySelector('#blueberries .remove-item');
+
+const quantityOrange = document.querySelector('#orange .quantity');
+const quantityBlueberries = document.querySelector('#blueberries .quantity');
+
+const priceTotalOrange = document.querySelector('#orange .price-total');
+const priceTotalBlueberries = document.querySelector('#blueberries .price-total');
+
+let quantOrange = 1;
+let quantBlueberries = 1;
+quantityOrange.innerHTML = quantOrange;
+priceTotalOrange.innerHTML = priceOrange;
+
+quantityBlueberries.innerHTML = quantBlueberries;
+priceTotalBlueberries.innerHTML = priceBlueberries;
+
+buttonAddItemOrange.addEventListener('click',
+  function () {
+    // quantityOrange.innerHTML = quantityOrange.innerHTML + 1;
+    // quantityOrange.innerHTML = parseFloat(quantityOrange.innerHTML) + 1;
+    // quantityOrange.innerHTML += 1;
+    quantOrange += 1
+    quantityOrange.innerHTML = quantOrange;
+
+    priceTotalOrange.innerHTML = quantOrange * priceOrange;
+  }
+)
+
+buttonRemoveItemOrange.addEventListener('click',
+  function () {
+    quantOrange -= 1
+    quantityOrange.innerHTML = quantOrange;
+
+    priceTotalOrange.innerHTML = quantOrange * priceOrange;
+  }
+)
+
+buttonAddItemBlueberries.addEventListener('click',
+  function () {
+    quantBlueberries += 1;
+    quantityBlueberries.innerHTML = quantBlueberries;
+
+    priceTotalBlueberries.innerHTML = quantBlueberries * priceBlueberries;
+  }
+)
+
+buttonRemoveItemBlueberries.addEventListener('click',
+  function () {
+    quantBlueberries -= 1;
+    quantityBlueberries.innerHTML = quantBlueberries;
+
+    priceTotalBlueberries.innerHTML = quantBlueberries * priceBlueberries;
+  }
+)
--- ex-js-dom-consegna/hamburger/js/script.js
+++ ex-js-dom-consegna/hamburger/js/script.js
@@ -1,1 +1,17 @@
 //Crea uno script che premendo sul bottone hamburger, mostra il menu mobile, e premendo su una X chiude il menu mobile
+
+const menu = document.getElementById('menu');
+const buttonOpen = document.getElementById('hamburger');
+const buttonClose = document.getElementById('close');
+
+buttonOpen.addEventListener('click',
+  function () {
+    menu.classList.add('active');
+  }
+)
+
+buttonClose.addEventListener('click',
+  function () {
+    menu.classList.remove('active');
+  }
+)
