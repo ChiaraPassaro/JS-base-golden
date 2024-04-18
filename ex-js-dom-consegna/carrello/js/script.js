0899b03601d0edc9b930d8602837c171b4d83f95
ele141082 <egn1982@gmail.com>
2024-04-18 18:57:36 +0200

    feat: added if and else

--- ex-js-dom-consegna/carrello/js/script.js
+++ ex-js-dom-consegna/carrello/js/script.js
@@ -4,53 +4,63 @@
 
 //Bonus: Aggiungi una riga con il prezzo totale di tutti i prodotti nel carrello
 
-alert ("ciao ciao")
+//alert ("ciao ciao")
 
-const orange = document.getElementById("orange");
-console.log("orange");
+//const orange = document.getElementById("orange");
+//console.log("orange");
 
-const blueberries = document.getElementById("blueberries");
-console.log("blueberries");
+//const blueberries = document.getElementById("blueberries");
+//console.log("blueberries");
 
-const buttonAdd = document.querySelector("btn");
-console.log("buttonAdd");
+//const buttonAdd = document.querySelector("btn");
+//console.log("buttonAdd");
 
-const quantityOrange = 1;
+//const quantityOrange = 1;
 
-const priceOrange = 3;
+//const priceOrange = 3;
 
 
 
-const temperatura = prompt("inserisci temperatura");
+//const temperatura = prompt("inserisci temperatura");
 
-if (temperatura < 0) {
-    console.log("è sotto lo zero");
+//if (temperatura < 0) {
+    //console.log("è sotto lo zero");
 
-} if(temperatura > 30){
-    console.log("é sopra i 30 gradi");
+//} if(temperatura > 30){
+    //console.log("é sopra i 30 gradi");
+
+//}else {
+    //console.log("temperatura normale");
+//}
+
+//const parola1 = prompt("inserisci una stringa");
+//const parola2 = prompt("inserisci una stringa")
+
+//if (parola1 === parola2){
+    //console.log("le stringhe sono uguali");
+
+//} else {
+    //console.log("le stringhe sono diverse");
+//}
+
+//const number = prompt("inserisci un numero");
+
+//if (number > 0 ) {
+    //console.log("il numero è positivo");
+
+//}else if (number < 0 ) {
+    //console.log("il numero è negativo");
+
+//}else {
+    //console.log("il numero è zero");
+//}
+
+const codiceFiscale = prompt("inserisci il tuo codice fiscale");
+
+
+if (codiceFiscale === 16) {
+    console.log("Codice fiscale valido");
 
 }else {
-    console.log("temperatura normale");
-}
-
-const parola1 = prompt("inserisci una stringa");
-const parola2 = prompt("inserisci una stringa")
-
-if (parola1 === parola2){
-    console.log("le stringhe sono uguali");
-
-} else {
-    console.log("le stringhe sono diverse");
-}
-
-const number = prompt("inserisci un numero");
-
-if (number > 0 ) {
-    console.log("il numero è positivo");
-
-}else if (number < 0 ) {
-    console.log("il numero è negativo");
-
-}else {
-    console.log("il numero è zero");
+    console.log("Codice fiscale non valido");
 }
