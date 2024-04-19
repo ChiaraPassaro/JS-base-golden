35779e0b5a1623db74d7150c2dd560097445d604
ChiaraPassaro <chiarapassaro.wd@gmail.com>
2024-04-19 18:24:52 +0200

    feat: added base script

---
+++ js-fruits/js/script.js
@@ -0,0 +1,14 @@
+//prima parte
+//se seleziono la checkbox su id orange si visualizza div.orange
+//se seleziono la checkbox su id apple si visualizza div.apple
+//se seleziono la checkbox su id blueberries si visualizza div.blueberries 
+//per avviare l'animazione devo cliccare sul bottone select
+//se clicco sul bottone reset si resetta tutto
+//se clicco sul bottone selectAll si selezionano tutte le checkbox
+
+
+//seconda parte
+//se seleziono orange e apple vinco un blueberries
+// se seleziono solo blueberries faccio comparire un messaggio in cui proponiamo all'utente di selezionare un altro prodotto
+
+// se clicco su buy controllo che il quantitativo sia di almeno 1 e non più di 10 per ogni prodotto selezionato.
---
+++ js-guess-number/js/script.js
@@ -0,0 +1,10 @@
+// 1. Genera due numeri random e stampa in pagina il maggiore
+
+// 4. Genera un numero casuale tra 1 e 100 e stampa se è pari o dispari.
+
+
+//genera un numero causale
+//chiedi all'utente di inserire un numero
+//se ha indovinato il numero stampa "Hai vinto"
+//se ha inserito un numero più grande di 1 o più piccolo di 1 "ci sei andato vicino"
+//oppure "Hai Perso"
